a1c4c69c4c77a6b3e155e4a0602d5df410714e2c
Vergelsky <graf.werger@gmail.com>
2024-02-28 15:47:57 +0700

    Создал приложение Blog и модель поста

--- mailing/models.py
+++ mailing/models.py
@@ -14,17 +14,17 @@ class Mailing(models.Model):
     send_time = models.TimeField(auto_now=True, verbose_name='Время рассылки')
     sends = models.IntegerField(default=0, verbose_name='Отправлено раз')
     emails = models.TextField(max_length=3000, verbose_name='Список адресов')
-    owner = models.ForeignKey('User', on_delete=models.CASCADE(), verbose_name='Создатель рассылки')
+    owner = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='Создатель рассылки')
 
 
 class Log(models.Model):
     last_try_dt = models.DateTimeField(auto_now=True, verbose_name='Время последней рассылки')
     result = models.CharField(max_length=600, verbose_name='Результаты выполнения')
-    maling = models.ForeignKey('Mailing', on_delete=models.CASCADE(), verbose_name='Рассылка')
+    mailing = models.ForeignKey('Mailing', on_delete=models.CASCADE, verbose_name='Рассылка')
 
 
 class Letter(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
     title = models.CharField(max_length=200, verbose_name='Заголовок')
     text = models.TextField(max_length=1000, verbose_name='Содержимое')
-    owner = models.ForeignKey('User', on_delete=models.CASCADE(), verbose_name='Создатель')
+    owner = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='Создатель')
