6668048a87aeff0d078149941b74fa989d1f6dbe
Vergelsky <graf.werger@gmail.com>
2024-02-28 15:19:42 +0700

    Создал приложение mailing и модель рассылки

--- users/models.py
+++ users/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='Почта')
-    name = models.CharField(unique=True, verbose_name='Имя пользователя')
-    about = models.CharField(blank=True, null=True, verbose_name='О себе')
+    name = models.CharField(max_length=80, unique=True, verbose_name='Имя пользователя')
+    about = models.CharField(max_length=600, verbose_name='О себе', blank=True, null=True)
     is_manager = models.BooleanField(default=False, verbose_name='Это менеджер')
